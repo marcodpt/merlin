eb77d73efec5a6de10cf43cd02ecbf0c0cfc8037
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2022-12-09 15:12:48 -0300

    api review and more examples

--- index.js
+++ index.js
@@ -3,20 +3,36 @@ import tint from 'https://cdn.jsdelivr.net/gh/marcodpt/tint/superfine.js'
 export default ({
   node,
   data,
-  actions
+  init,
+  drop
 }) => {
-  const state = typeof data == 'function' ? data() : data
   const patch = tint(node)
-  const render = () => patch(state)
-  if (actions && (state != null && typeof state == 'object')) {
-    const f = actions(state, render)
-    Object.keys(f).forEach(key => {
-      state[key] = function () {
-        const res = f[key].apply(null, arguments)
-        render()
+  var stop = false
+  const render = () => !stop && patch(data)
+  render()
+  if (typeof init == 'function' && (data && typeof data == 'object')) {
+    const f = init(data, render)
+    if (f && typeof f == 'object') {
+      Object.keys(f).forEach(key => {
+        data[key] = typeof f[key] == 'function' ? function () {
+          if (!stop) {
+            const res = f[key].apply(null, arguments)
+            render()
+            return res
+          }
+        } : f[key]
+      })
+      render()
+    }
+  }
+  return () => {
+    if (!stop) {
+      if (typeof drop == 'function') {
+        const res = drop(data)
+        stop = true
         return res
       }
-    })
+      stop = true
+    }
   }
-  render()
 }
