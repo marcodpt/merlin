3d5492141bc2a3ab7e0469c84499e3ea32f31e8c
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2022-12-13 19:15:52 -0300

    Improve a lot many things

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 import tint from 'https://cdn.jsdelivr.net/gh/marcodpt/tint/superfine.js'
 
 export default ({
-  node,
-  data,
+  template,
   init,
   drop
-}) => {
-  const patch = tint(node)
+}) => (node, params) => {
+  const patch = tint(node, template)
   var stop = false
-  const render = () => !stop && patch(data)
-  render()
-  if (typeof init == 'function' && (data && typeof data == 'object')) {
-    const f = init(data, render)
-    if (f && typeof f == 'object') {
-      Object.keys(f).forEach(key => {
-        data[key] = typeof f[key] == 'function' ? function () {
-          if (!stop) {
-            const res = f[key].apply(null, arguments)
-            render()
-            return res
-          }
-        } : f[key]
+  const state = {}
+  const merge = x => {
+    if (x && typeof x == 'object') {
+      Object.keys(x).forEach(k => {
+        state[k] = x[k]
       })
-      render()
     }
   }
-  return () => {
+  const update = callback => {
     if (!stop) {
-      if (typeof drop == 'function') {
-        const res = drop(data)
-        stop = true
-        return res
+      if (typeof callback == 'function') {
+        merge(callback(state))
+      } else {
+        merge(callback)
       }
-      stop = true
+      patch(state)
     }
+  }
+
+  if (typeof init == 'function') {
+    update(init(update, params))
+  } else {
+    update()
+  } 
+
+  return () => {
+    if (!stop && typeof drop == 'function') {
+      drop(state)
+    }
+    stop = true
   }
 }
---
+++ samples/data.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    "name": "Roman Patrick",
+    "age": 33,
+    "balance": 2923.74
+  },
+  {
+    "name": "Moon Fischer",
+    "age": 35,
+    "balance": 3605.23
+  },
+  {
+    "name": "Kayla Odom",
+    "age": 37,
+    "balance": 3467.08
+  },
+  {
+    "name": "Susie Hawkins",
+    "age": 31,
+    "balance": 2941.08
+  },
+  {
+    "name": "Fern Donovan",
+    "age": 23,
+    "balance": 1784.53
+  },
+  {
+    "name": "Spence Nelson",
+    "age": 34,
+    "balance": 1756.61
+  },
+  {
+    "name": "Everett Owens",
+    "age": 30,
+    "balance": 1903.71
+  },
+  {
+    "name": "Guerra Mcdaniel",
+    "age": 21,
+    "balance": 1769.37
+  },
+  {
+    "name": "Lorene Allen",
+    "age": 39,
+    "balance": 2593.91
+  },
+  {
+    "name": "Wolfe Bradshaw",
+    "age": 27,
+    "balance": 2306.55
+  }
+]
